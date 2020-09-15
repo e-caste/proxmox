f6fc6d2702056b044dac7aa0afcc8cd6f46ed2df
e-caste-mbp <en.cas.dev@icloud.com>
2020-09-15 18:15:16 +0200

    move vars to dedicated file

--- cloud-init-template.sh
+++ cloud-init-template.sh
@@ -6,34 +6,8 @@
 # exit at first error
 set -e
 
-# see http://cloud-images.ubuntu.com
-# e.g. bionic, focal
-UBUNTU_VER=
-# where to download the cloud image
-# e.g. /mnt/pve/freenas/tmp, .
-DL_PATH=
-# the template VM number ID
-# e.g. 9000
-VM_NUM=
-# the number of cores of the template VM
-# e.g. 1, 2, 4, 64
-VCPUS=
-# the Mebibytes of memory of the template VM
-# e.g. 512, 1024, 2048, 8192
-MEM_MIB=
-# the disk size of the template VM
-# e.g. 16G, 64G
-DISK_SIZE=
-# the destination storage ID, where the disk will be stored
-# it is the string in the left panel of the web dashboard
-# e.g. local-lvm, local, freenas
-DST_STORAGE_ID=
-# the default username of the template VM
-# e.g. ubuntu, johnny
-USERNAME=
-# the path to the cloud-init yaml user config file
-# e.g. freenas:snippets/user.yaml
-CI_YAML_PATH=
+# set all needed vars in ci-vars.sh
+source ci-vars.sh
 
 # download cloud image (only if it's newer than the already downloaded one)
 wget https://cloud-images.ubuntu.com/$UBUNTU_VER/current/$UBUNTU_VER-server-cloudimg-amd64.img --directory-prefix=$DL_PATH --timestamping
