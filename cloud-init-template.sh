d341a79b2e8f433a74021380d0217374113f54d7
e-caste-mbp <en.cas.dev@icloud.com>
2020-09-15 18:13:37 +0200

    make use of cloud-init yaml

--- cloud-init-template.sh
+++ cloud-init-template.sh
@@ -31,9 +31,9 @@ DST_STORAGE_ID=
 # the default username of the template VM
 # e.g. ubuntu, johnny
 USERNAME=
-# the path to your id_rsa_X.pub files, one per line
-# e.g. /root/id_rsa_proxmox.pub
-SSHKEYS_PATH=
+# the path to the cloud-init yaml user config file
+# e.g. freenas:snippets/user.yaml
+CI_YAML_PATH=
 
 # download cloud image (only if it's newer than the already downloaded one)
 wget https://cloud-images.ubuntu.com/$UBUNTU_VER/current/$UBUNTU_VER-server-cloudimg-amd64.img --directory-prefix=$DL_PATH --timestamping
@@ -42,17 +42,18 @@ wget https://cloud-images.ubuntu.com/$UBUNTU_VER/current/$UBUNTU_VER-server-clou
 qm create $VM_NUM --cores $VCPUS --memory $MEM_MIB --net0 virtio,bridge=vmbr0
 qm importdisk $VM_NUM $DL_PATH/$UBUNTU_VER-server-cloudimg-amd64.img $DST_STORAGE_ID
 qm set $VM_NUM --scsihw virtio-scsi-pci --scsi0 $DST_STORAGE_ID:$VM_NUM/vm-$VM_NUM-disk-0.raw
+qm set $VM_NUM --agent enabled=1
 # add CDROM to pass cloud-init data
 qm set $VM_NUM --ide2 $DST_STORAGE_ID:cloudinit
 qm set $VM_NUM --boot c --bootdisk scsi0
 qm set $VM_NUM --serial0 socket --vga serial0
 
 # set cloudinit options
-qm set $VM_NUM --ciuser $USERNAME \
-        --ipconfig0 "ip=dhcp" \
-        --sshkeys $SSHKEYS_PATH
+qm set $VM_NUM  --ipconfig0 "ip=dhcp" \
+        --cicustom "user=$CI_YAML_PATH"
 
 # create template from this VM
+qm cloudinit dump $VM_NUM user
 qm resize $VM_NUM scsi0 $DISK_SIZE
 qm set $VM_NUM --name ubuntu-server-$UBUNTU_VER
 qm template $VM_NUM
